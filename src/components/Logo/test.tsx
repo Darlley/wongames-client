0f9b681084ad3aebac02df49667dfdf7cd02f70c
Darlley <systembrtt@gmail.com>
2025-02-16 16:47:49 -0400

    aula 74: teste da prop size do componente Logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -18,4 +18,18 @@ describe('<Logo />', () => {
       color: theme.colors.black
     })
   })
+
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
 })
