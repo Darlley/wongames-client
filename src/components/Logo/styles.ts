3f75e2e3c4104b3dee61655b9c7a26d0847c3bc2
Darlley <systembrtt@gmail.com>
2025-02-16 17:19:53 -0400

    aula 75

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,7 +1,9 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+
 import { LogoProps } from '.'
 
-const WrapperModifier = {
+const wrapperModifiers = {
   normal: () => css`
     width: 11rem;
     height: 3.3rem;
@@ -10,12 +12,30 @@ const WrapperModifier = {
   large: () => css`
     width: 20rem;
     height: 5.9rem;
+  `,
+
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .text {
+        display: none;
+      }
+    `}
   `
 }
 
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
-    ${!!size && WrapperModifier[size]};
+
+    ${!!size && wrapperModifiers[size]}
+    ${hideOnMobile && wrapperModifiers.hideOnMobile}
   `}
 `
