4000c5c1d48697c0fb7d0f46b2fede7b96c26b6e
Darlley <systembrtt@gmail.com>
2025-02-17 22:17:36 -0400

    criação de componente e primeiros estilos css

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './styles'
+
+export type HeadingProps = {
+  children: React.ReactNode
+}
+
+const Heading = ({ children }: HeadingProps) => (
+  <S.Wrapper>{children}</S.Wrapper>
+)
+
+export default Heading
