adb3568601d24cda6c5ca8652c8b180624daae3e
Darlley <systembrtt@gmail.com>
2025-02-18 17:26:22 -0400

    aula 76

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,12 +1,37 @@
-import styled, { css } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
+import { HeadingProps } from '.'
 
-export const Wrapper = styled.div`
-  ${({ theme }) => css`
+const wrapperModifiers = {
+  lineLeft: (theme: DefaultTheme) => css`
+    padding-left: ${theme.spacings.xxsmall};
+    border-left: 0.7em solid ${theme.colors.secondary};
+  `,
+  lineBottom: (theme: DefaultTheme) => css`
+    position: relative;
+    margin-bottom: ${theme.spacings.medium};
+
+    &::after {
+      position: absolute;
+      left: 0;
+      content: '';
+      width: 5rem;
+      bottom: -1rem;
+      border: 0.4rem solid ${theme.colors.primary};
+    }
+  `
+}
+
+export const Wrapper = styled.h2<HeadingProps>`
+  ${({ theme, color, lineLeft, lineBottom }) => css`
     font-size: ${theme.font.sizes.xlarge};
+    color: ${theme.colors[color!]};
 
     ${media.greaterThan('medium')`
       font-size: ${theme.font.sizes.xxlarge};
     `}
+
+    ${lineLeft && wrapperModifiers.lineLeft(theme)}
+    ${lineBottom && wrapperModifiers.lineBottom(theme)}
   `}
 `
