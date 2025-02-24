c1bd208fd83706d6351c7d94c990e2ecf089e25d
Darlley <systembrtt@gmail.com>
2025-02-24 13:27:23 -0400

    aula 81

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -6,7 +6,6 @@ import { renderWithTheme } from 'utils/tests/helpers'
 describe('<Button />', () => {
   it('should render the medium size by default', () => {
     renderWithTheme(<Button>Buy now</Button>)
-
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       height: '4rem',
       padding: '0.8rem 3.2rem',
@@ -16,7 +15,6 @@ describe('<Button />', () => {
 
   it('should render the small size by default', () => {
     renderWithTheme(<Button size='small'>Buy now</Button>)
-
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       height: '3rem',
       'font-size': '1.2rem'
@@ -25,11 +23,17 @@ describe('<Button />', () => {
 
   it('should render the large size by default', () => {
     renderWithTheme(<Button size='large'>Buy now</Button>)
-
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       height: '5rem',
       padding: '0.8rem 4.8rem',
       'font-size': '1.6rem'
+    })
+  })
+
+  it('should render a fullWidth version', () => {
+    renderWithTheme(<Button fullWidth>Buy now</Button>)
+    expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
+      width: '100%'
     })
   })
 })
