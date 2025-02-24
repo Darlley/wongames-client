7622b9e9928f48d658b93f31156710977258d709
Darlley <systembrtt@gmail.com>
2025-02-24 14:06:22 -0400

    aula 82

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -6,12 +6,13 @@ import { AddShoppingCart } from '@styled-icons/material-outlined'
 
 describe('<Button />', () => {
   it('should render the medium size by default', () => {
-    renderWithTheme(<Button>Buy now</Button>)
+    const { container } = renderWithTheme(<Button>Buy now</Button>)
     expect(screen.getByRole('button', { name: /Buy now/i })).toHaveStyle({
       height: '4rem',
       padding: '0.8rem 3.2rem',
       'font-size': '1.4rem'
     })
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render the small size by default', () => {
