a110fe738f5a24756beeeda209b3409e1dd293ce
Darlley <systembrtt@gmail.com>
2025-03-03 20:54:16 -0400

    aula 93

---
+++ src/components/Footer/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+
+import Footer from '.'
+
+describe('<Footer />', () => {
+  // TODO: FAZER OS TESTES
+  it('should render 4 columns topics', () => {
+    // expect coluna contato
+    // expect coluna follow us
+    // expect coluna links
+    // expect coluna location
+  })
+})
