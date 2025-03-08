658692178c354394272888bb085b70e1251b8134
Darlley <systembrtt@gmail.com>
2025-03-07 20:32:19 -0400

    aula 97

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -52,34 +52,34 @@ describe('<Heading />', () => {
   })
 
   it('should render a Heading with a primary line color', () => {
-    renderWithTheme(<Heading lineColor="primary" lineBottom>Won Games</Heading>)
+    renderWithTheme(
+      <Heading lineColor="primary" lineBottom>
+        Won Games
+      </Heading>
+    )
     const heading = screen.getByRole('heading', { name: /won games/i })
-  
+
     expect(heading).toHaveStyle({
       'border-left': '0.7rem sikud #F231A5'
     })
-    expect(heading).toHaveStyleRule(
-      'border-bottom',
-      '0.5rem solid #F231A5',
-      {
-        modifier: '::after'
-      }
-    )
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after'
+    })
   })
 
   it('should render a Heading with a secondary line color', () => {
-    renderWithTheme(<Heading lineColor="secondary" lineBottom>Won Games</Heading>)
+    renderWithTheme(
+      <Heading lineColor="secondary" lineBottom>
+        Won Games
+      </Heading>
+    )
     const heading = screen.getByRole('heading', { name: /won games/i })
-  
+
     expect(heading).toHaveStyle({
       'border-left': '0.7rem sikud #3CD3C1'
     })
-    expect(heading).toHaveStyleRule(
-      'border-bottom',
-      '0.5rem solid #3CD3C1',
-      {
-        modifier: '::after'
-      }
-    )
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
+      modifier: '::after'
+    })
   })
 })
