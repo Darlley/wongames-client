74ed88443826dfe4a1d3debdfedc03c9a0f89ce6
Darlley <systembrtt@gmail.com>
2025-03-11 22:01:43 -0400

    aula 119

--- src/components/BannerSlider/test.tsx
+++ src/components/BannerSlider/test.tsx
@@ -6,7 +6,7 @@ import BannerSlider from '.'
 
 const items = [
   {
-    img: 'https://images.unsplash.com/photo-1553481187-be93c21490a9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    img: 'https://images.unsplash.com/photo-1602620502036-e52519d58d92?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D',
     title: 'Defy death 1',
     subtitle: '<p>Play the new <strong>CrashLands</strong> season',
     buttonLabel: 'Buy now',
@@ -14,8 +14,15 @@ const items = [
     ribbon: 'Bestselling'
   },
   {
-    img: 'https://images.unsplash.com/photo-1602620502036-e52519d58d92?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D',
+    img: 'https://images.unsplash.com/photo-1553481187-be93c21490a9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1602620502036-e52519d58d92?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D',
+    title: 'Defy death 3',
     subtitle: '<p>Play the new <strong>CrashLands</strong> season',
     buttonLabel: 'Buy now',
     buttonLink: '/games/defy-death'
@@ -32,7 +39,7 @@ describe('<BannerSlider />', () => {
   it('should render with 1 active item', () => {
     const { container } = renderWithTheme(<BannerSlider items={items} />)
 
-    expect(container.querySelectorAll('.slick-slide')).toHaveLength(2)
+    expect(container.querySelectorAll('.slick-slide')).toHaveLength(3)
     expect(container.querySelectorAll('li.slick-active')).toHaveLength(1)
 
     expect(
@@ -45,8 +52,9 @@ describe('<BannerSlider />', () => {
   })
   
   it('should render the dots', () => {
-    const { container, debug } = renderWithTheme(<BannerSlider items={items} />)
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
 
-    expect(container.querySelectorAll("ul.slick-dots li")).toHaveLength(2)
+    expect(container.querySelector("ul.slick-dots")).toBeInTheDocument()
+    expect(container.querySelectorAll("ul.slick-dots li")).toHaveLength(3)
   })
 })
