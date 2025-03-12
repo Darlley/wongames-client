d4b8c8bb3f6e9681aa5269b8ca9387b3bdbe981d
Darlley <systembrtt@gmail.com>
2025-03-12 16:00:03 -0400

    aula 128

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,7 +1,44 @@
+import { Container } from 'components/Container'
 import * as S from './styles' 
+import Menu from 'components/Menu'
+import Heading from 'components/Heading'
+import Footer from 'components/Footer'
+
 const Home = () => (
   <S.Wrapper>
-    <h1>Home</h1>
+    <section>
+    <Container>
+      <Menu />
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="black">
+        News
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Most Popular
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Upcomming
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Free Games
+      </Heading>
+    </Container>
+
+    <Container>
+      <Footer />
+    </Container>
+  </section>
   </S.Wrapper>
 ) 
 export default Home
