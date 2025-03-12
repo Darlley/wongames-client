cbbe933beebe1b0631837588a211f6170395ed95
Darlley <systembrtt@gmail.com>
2025-03-12 17:56:16 -0400

    aula 133

--- src/components/GameCardSlider/mock.ts
+++ src/components/GameCardSlider/mock.ts
@@ -34,11 +34,4 @@ export default [
     price: 'R$ 235,00',
     promotionalPrice: 'R$ 215,00'
   },
-  {
-    title: 'Population Zero',
-    developer: 'Rockstar Games',
-    img: 'https://images.unsplash.com/photo-1602620502036-e52519d58d92?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D',
-    price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
-  }
 ]
--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,20 +1,38 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
+import bannerMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
 import Home from '.'
+
+const props = {
+  banners: bannerMock,
+  newGames: [gamesMock[0]],
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: [gamesMock[0]],
+  upcommingGames: [gamesMock[0]],
+  upcommingHighligth: highlightMock,
+  upcommingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
+  freeHighligth: highlightMock
+}
 
 describe('<Home />', () => {
   it('should render menu and footer', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(
-      screen.getByRole('heading', { name: /contact us/i })
+      screen.getByRole('heading', { name: /follow us/i })
     ).toBeInTheDocument()
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
   })
 
-  it('should render the sections', () => {
-    renderWithTheme(<Home />)
+  it('should render sections', () => {
+    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
     expect(
@@ -26,5 +44,14 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument()
+  })
+
+  it('should render section elements', () => {
+    renderWithTheme(<Home {...props} />)
+    
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+    // card game ( 5 sections com 1 card cada = 5x1 = 5)
+    // expect(screen.getAllByText(/Population Zero/i)).toHaveLength(5) received 10
+    expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3)
   })
 })
