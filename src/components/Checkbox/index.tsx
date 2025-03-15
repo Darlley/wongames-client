6f11fe2fd7993bf606dc92e4c4f6627dfb4bf64f
Darlley <systembrtt@gmail.com>
2025-03-15 19:55:34 -0400

    aula 144

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -3,32 +3,41 @@ import * as S from './styles'
 
 export type CheckboxProps = {
   onCheck?: (status: boolean) => void
+  isChecked?: boolean
   label?: string
   htmlFor: string
   labelColor?: 'white' | 'black'
+  value?: string | ReadonlyArray<string> | number
 } & InputHTMLAttributes<HTMLInputElement>
 
 const Checkbox = ({
   onCheck,
+  isChecked = false,
   label,
   htmlFor,
-  labelColor = 'white'
+  labelColor = 'white',
+  value,
+  ...props
 }: CheckboxProps) => {
-  const [checked, setChecked] = useState(false)
+  const [checked, setChecked] = useState(isChecked)
 
   const onChange = () => {
     const status = !checked // true => false => true
     setChecked(status)
 
-    if (onCheck) {
-      onCheck(status)
-    }
+    !!onCheck && onCheck(status)
   }
 
   return (
     <S.Wrapper>
-      <S.Input id={htmlFor} type="checkbox" onChange={onChange}
-        checked={checked} />
+      <S.Input 
+        id={htmlFor} 
+        type="checkbox" 
+        onChange={onChange} 
+        checked={checked} 
+        value={value} 
+        {...props} 
+      />
       {label && <S.Label htmlFor={htmlFor} labelColor={labelColor}>{label}</S.Label>}
     </S.Wrapper>
   )
