c3fea626a32ab69380e21f36438fe0c8e58bac32
Darlley <systembrtt@gmail.com>
2025-03-15 18:52:22 -0400

    aula 139

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,8 @@
+import * as S from './styles' 
+const Checkbox = () => (
+  <S.Wrapper>
+    <input type='checkbox' id="action" />
+    <label htmlFor="action">Action</label>
+  </S.Wrapper>
+) 
+export default Checkbox
