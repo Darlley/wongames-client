bf4751caac806cfd36fd4f9968f0c34475579228
Darlley <systembrtt@gmail.com>
2025-04-08 11:47:56 -0400

    aula 179

--- src/components/GameDatails/test.tsx
+++ src/components/GameDatails/test.tsx
@@ -4,7 +4,7 @@ import GameDatails from '.'
 
 describe('<GameDatails />', () => {
   it('should render the heading', () => {
-    const { container } = render(<GameDatails />)
+    const { container } = render(<GameDatails platforms={['windows', 'linux', 'mac']} />)
 
     expect(screen.getByRole('heading', { name: /GameDatails/i })).toBeInTheDocument()
 
