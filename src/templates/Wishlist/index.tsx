749725bd648f78cfa1b33e42cb47f0bd2bb98c9d
Darlley <systembrtt@gmail.com>
2025-04-19 19:34:06 -0400

    aula 207

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -1,7 +1,35 @@
-import * as S from './styles' 
-const Wishlist = () => (
-  <S.Wrapper>
-    <h1>Wishlist</h1>
-  </S.Wrapper>
-) 
+import Base from 'templates/Base'
+
+import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
+import { Container } from 'components/Container'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+
+import * as S from './styles'
+
+export type WishlistTemplateProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+}
+
+const Wishlist = ({
+  recommendedGames,
+  recommendedHighlight
+}: WishlistTemplateProps) => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Wishlist
+      </Heading>
+    </Container>
+
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
+  </Base>
+)
+
 export default Wishlist
