26cd9212ab7956d31e11650388574c0c7a36404f
Darlley <systembrtt@gmail.com>
2025-04-22 12:18:36 -0400

    aula 215

--- src/components/GameItem/stories.tsx
+++ src/components/GameItem/stories.tsx
@@ -5,7 +5,7 @@ export default {
   title: 'GameItem',
   component: GameItem,
   args: {
-    img: 'https://source.unsplash.com/user/willianjusten/151x70',
+    img: 'https://images.unsplash.com/photo-1674044164226-719e76fa644b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'Red Dead Redemption 2',
     price: 'R$ 215,00'
   }
