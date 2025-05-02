165e52b269451f9c76c57e669e6d87fae0df23c1
Darlley <systembrtt@gmail.com>
2025-05-02 18:30:45 -0400

    aula 279

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -1,27 +1,12 @@
 import { gql } from "@apollo/client";
+import { BannerFragment } from "graphql/fragments/banner.fragment";
 
 export const QUERY_HOME = gql`
   query QueryHome {
     banners {
-      image {
-        url
-      }
-      title
-      subtitle
-      button {
-        label
-        link
-      }
-      ribbon {
-        text
-        color
-        size
-      }
-      game_link {
-        slug
-      }
+      ...BannerFragment
     }
   }
+
+  ${BannerFragment}
 `
-
-
